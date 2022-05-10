5f6f1dc892967bec392278c57f78c3c8bcf8a199
Rene <36115705+rene78@users.noreply.github.com>
2022-05-10 16:18:46 +0200

    New countries: ng, br, mx, bd, id, pk, ru

--- data/regions.js
+++ data/regions.js
@@ -1,9 +1,9 @@
 //All continents/states/provinces to play. Needed to build the selection container and the highscore list.
 //Names have to be similar as the geojson-files in /data 
 export const regions = [
-  ["africa", "za"],
-  ["america", "us"],
-  ["asia", "cn", "in", "my"],
+  ["africa", "ng", "za"],
+  ["america", "br", "mx" , "us"],
+  ["asia", "bd", "cn", "id", "in", "my", "pk"],
   ["australia", "au"],
-  ["europe", "de", "fr"]
+  ["europe", "de", "fr", "ru"]
 ];
